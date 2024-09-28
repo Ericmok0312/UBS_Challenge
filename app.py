8ec30c529f35a2a624f9670689c7230069bdc0e8
bestfranklinAI <franklin123ann@gmail.com>
2024-09-28 12:00:05 +0800

    init

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import logging
+import socket
 
-app = Flask(__name__)
+from routes import app
 
-@app.route('/')
-def base_route():
-    return "successful"
+logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/', methods=['GET'])
+def default_route():
+    return 'Python Template'
+
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    logging.info("Starting application ...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 8080))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=port)
