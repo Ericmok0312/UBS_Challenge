bea7df7a83306cc6332884ab4b4251edb17acb4a
Eric <weemokkachun@gmail.com>
2024-09-28 15:31:28 +0800

    hunter

--- routes/kazuma.py
+++ routes/kazuma.py
@@ -7,44 +7,27 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-def kazuma_solver(monsters):
-    nums = []
-    
-    if len(monsters) < 2:
-        return 0
-    
-    for i in range(1, len(monsters)):
-        nums.append(monsters[i] - monsters[i-1])
-    
-    n = len(nums)
-    if n <= 2:
-        return max(sum(nums), 0)
-    
-    dp = [[0, 0] for _ in range(n)]
-    
-    # Base cases
-    dp[0][0] = nums[0]
-    dp[0][1] = 0
-    
-    # Fill dp table
-    for i in range(1, n):
-        if i >= 2:
-            dp[i][0] = max(dp[i-1][0], dp[i-2][0] + nums[i])
-        else:
-            dp[i][0] = max(dp[i-1][0], 0)
+
+def kazuma_solver(monsters, efficiency, can_shoot):
+    if (len(monsters)==0):
+        return efficiency
+    else:
+        if(not(can_shoot)):
+            res1 = kazuma_solver(monsters[1:], efficiency-monsters[0], not can_shoot) # spell
+            res2 = kazuma_solver(monsters[1:], efficiency, can_shoot)
+            res3 = -999
+        elif(can_shoot):
+            res1 = kazuma_solver(monsters[2:], efficiency+monsters[0], not can_shoot) #shoot 
+            res2 = kazuma_solver(monsters[1:], efficiency-monsters[0], can_shoot)
+            res3 = kazuma_solver(monsters[1:], efficiency, can_shoot)
         
-        dp[i][1] = max(dp[i-1][1], dp[i-2][1])
-    
-    return max(dp[-1])
-
-
-monsters = [1, 6, 17, 11, 12, 14]
-efficiency = kazuma_solver(monsters)
-print(efficiency)
+        return max(res1, res2,res3)
 
 @app.post('/wordle-game')
 def solve_kazuma():
     data = request.get_json()
     monsters = data.get("monsters")
-    efficinecy = kazuma_solver(monsters)
-    return {"efficiency": efficiency}
+    ans = []
+    for i in range(len(monsters)):
+        ans.append({"efficiency": kazuma_solver(monsters, 0, false)})
+    return ans
