26dc0d11b1dc84b7efd67687c9503e7dee1c9ba3
Sunny Sze <szewangcheong@gmail.com>
2024-09-29 03:14:55 +0800

    many debug intepreter

--- routes/mini_interpreter.py
+++ routes/mini_interpreter.py
@@ -18,7 +18,10 @@ class interpreter:
     def puts(self, *message):
         if not (len(message) == 1):
             return False
-        # print(message[0])
+        message = list(message)
+        for i, val in enumerate(message):
+            if val in self.arguments.keys():
+                message[i] = self.arguments[val]
         return message[0]
 
     def assignment(self, *argname):
@@ -32,6 +35,10 @@ class interpreter:
     def st_concat(self, *arg):
         if(not(len(arg) == 2)):
             return False
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
         b = arg[1]
         if not (a[0]==a[-1] and a[0]=='"'):
@@ -44,6 +51,10 @@ class interpreter:
     def stlower(self, *arg):
         if(not(len(arg)==1)):
             return False
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
 
         a = arg[0]
 
@@ -57,7 +68,10 @@ class interpreter:
     def stupper(self, *arg):
         if(not(len(arg)==1)):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
 
         if not (a[0]==a[-1] and a[0]=='"'):
@@ -68,9 +82,12 @@ class interpreter:
 
 
     def substringReplace(self, *arg):
-        if(not len(arg==3)):
+        if(not len(arg) == 3):
             return False
-        
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
         b = arg[1]
         c = arg[2]
@@ -87,19 +104,23 @@ class interpreter:
         b= b[1:-1]
         c= c[1:-1]
 
-        loc= a.find(b)
-        if(loc == -1):
-            return arg[0]
+        # loc= a.find(b)
+        # if(loc == -1):
+        #     return arg[0]
+        return a.replace(b, c)
 
-        return '"' + a[:loc] + c + a[loc+len(c):] + '"'
+        # return '"' + a[:loc] + c + a[loc+len(c):] + '"'
 
         
 
 
     def substring(self, *arg):
-        if (not(len(arg==3))):
+        if (not(len(arg) == 3)):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
         b = arg[1]
         c = arg[2]
@@ -122,7 +143,10 @@ class interpreter:
 
         if(len(arg)<2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
                 ls.append(arg[i])
@@ -138,7 +162,10 @@ class interpreter:
     def sub(self, *arg):
         if(not(len(arg)==2)):
             return False
-        
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         ls = []
 
         for i in range(len(arg)):
@@ -156,7 +183,10 @@ class interpreter:
 
         if(len(arg)<2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
                 ls.append(arg[i])
@@ -181,7 +211,10 @@ class interpreter:
     def division(self, *arg):
         if(not(len(arg)==2)):
             return False
-        
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         ls = []
 
         for i in range(len(arg)):
@@ -191,7 +224,7 @@ class interpreter:
                 return False
         if (ls[1] == "0"):
             return False
-        if ('.' in ls[0] and '.' in ls[1]):
+        if ('.' not in ls[0] and '.' not in ls[1]):
             return str(int(ls[0])/int(ls[1]))
         return f"{float(ls[0])/float(ls[1]):.4g}"
 
@@ -199,7 +232,10 @@ class interpreter:
     def abosulte(self, *arg):
         if(not len(arg)==1):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         ls = []
 
         for i in range(len(arg)):
@@ -214,10 +250,13 @@ class interpreter:
 
     def getMax(self, *arg):
         ls = []
-
+        
         if(len(arg)<2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
                 ls.append(float(arg[i]))
@@ -232,7 +271,10 @@ class interpreter:
 
         if(len(arg)<2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
                 ls.append(float(arg[i]))
@@ -245,7 +287,10 @@ class interpreter:
     def greaterthan(self, *arg):
         if not(len(arg)==2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         ls = []
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
@@ -261,7 +306,10 @@ class interpreter:
     def lessthan(self, *arg):
         if not(len(arg)==2):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         ls = []
         for i in range(len(arg)):
             if (arg[i].replace('.', '', 1).isdigit() or arg[i].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit() or (arg[i][0] == '-' and arg[i][1:].replace('e', '', 1).replace('+', '').replace('-', '').replace('.', '', 1).isdigit())):
@@ -277,7 +325,10 @@ class interpreter:
     def equal(self, *arg):
         if (not(len(arg)==2)):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
         if arg[0][0]=='"' and arg[0][-1] == '"':
             a = a[0][1:-1]
@@ -294,7 +345,10 @@ class interpreter:
     def neq(self, *arg):
         if (not(len(arg)==2)):
             return False
-
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         a = arg[0]
         if arg[0][0]=='"' and arg[0][-1] == '"':
             a = a[0][1:-1]
@@ -310,7 +364,10 @@ class interpreter:
     def tostr(self, *arg):
         if not len(arg) == 1:
             return False
-        
+        arg = list(arg)
+        for i, val in enumerate(arg):
+            if val in self.arguments.keys():
+                arg[i] = self.arguments[val]
         return '"' + str(arg[0]) + '"'
 
     
@@ -359,7 +416,10 @@ class interpreter:
         index = 0
         statement = []
         current_statement = ""
+        inWord = False
         while index < len(input):
+            if input[index] == '\"':
+                inWord = not inWord
             if input[index] == '(':
                 result = self.interpreter_func(input[index + 1:])
                 if not result:
@@ -380,13 +440,13 @@ class interpreter:
                     statement.append(current_statement)
                     current_statement = ""
                 return self.translate(statement)
-            elif input[index] != ' ':
-                current_statement += input[index]
-                index += 1
-            else:
+            elif input[index] == ' ' and not inWord:
                 if current_statement != "":
                     statement.append(current_statement)
                     current_statement = ""
+                index += 1
+            else:
+                current_statement += input[index]
                 index += 1
         return statement
                 
@@ -454,7 +514,7 @@ data = {
 #                   '(puts (str (concat (concat "G3t:xxItx1y3k3hNWvj_uT>StiZjTO_7X4VNo>3zK@MCUSN>MPUZl35tTOo_>BGwYbdEihTTLBvR^GJyBjaFSX1N2we[^d4DlE<Jv`TRtBE1N]5ZeNwoCJyxZ@1[>GSjP3>EN`b8cc_j?W:fN_v_cU8nl1g8JEm4w81qWRz^qI5se:Cwq>rCx<OPQ:e[W0;4BZL>?" "3WQ>wWCcqAXPIzD]b?c;ln?m=aMdq@5y3F>?Qt]JLP=gAfUQ114oKwg`YavYUKI?]^8N6J]@G5N_QGq4rwZ0idUt6Am3lqL<i]TD_pgxEjKM_a<QG]kPk=M@EaFOQuLGl[h:=nB=3yyP9>uoOy>[9N4hQ") (str (divide -6709 0)))))'
 #                   ]
     # "expressions": ['(set integer 3571)', '(puts (str (min -6793.2542 -881.804 -5855.1282 9553.2677 -2827.2686 8653.1933 -4285.4174 8800.5244 -7400.1013 -9392.4838 -6999.3198 -9544.3795 2423.9715 2516.5775 5732.3224 9826.9646 9588.1015 -2658.8987 4991.013 3849.2933 9631.4483 -6177.8205 -9345.3672 -5853.149 8422.9775 5726.7487 7087.3448 -9922.5071 5946.7117 -7178.3715 -3991.7834 -377.8366 -3657.896 1687.6651 2540.4958 5082.1077 7712.6126 839.6264 -5175.3332 28.355 703.3311 9970.0534 4115.1373 -7307.3346 1860.0029 -8981.8619 -5818.5971 -4160.741 3799.067 -179.9052 -2020.4614 9208.5699 -5362.8407 -3517.038 -3330.5141 -7216.0994 5658.4787 -610.7228 6890.4184 4915.1224 -5567.1607 -7560.9721 5666.0896 1328.897 5377.4427 -4239.3325 9079.8601 -4242.0258 -7707.4722 -2412.2851 4842.0612 2362.5943 -4714.221 -5241.6083 -7537.6642 -5949.5655 7431.1831 1725.74 -3231.4161 8287.038 -5839.338 -4792.9912 -7278.1072 -2541.3327 4158.4178 5717.7764 9564.5959 8241.5069 -8558.0647 6844.6027 7016.0994 6822.7021 -52.4738 -5364.1955 -3219.896 -1917.4206 -3575.5406 9734.1196 5692.9863 3614.0299)))', '(puts (str (subtract 7040 -3797.2093)))', '(puts (str (divide 1559 4833.5726)))', '(puts (str (abs -7311)))', '(puts (str (max -7436.2597 6037.3712 -3848.9988 8774.8181 -5017.3059 -6947.0579 -2234.1073 -6076.1926 -997.5785 8322.4692 6681.2841 -2019.0622 8900.104 -1906.7632 -2583.0857 7309.0147 1700.8448 1586.8974 -5766.9873 -9022.2763 2990.1068 -670.649 -8031.1853 907.7643 8841.9054 -1497.6666 -6591.3394 -1403.0013 -7842.2969 1262.163 -3474.0647 -266.4496 965.9343 5731.0467 -9670.7417 8873.937 6058.5841 -4148.7329 -369.8817 -3116.6758 -7830.9433 8218.1773 -2113.576 3985.5103 -2041.4208 -827.8817 -9896.9745 5085.2356 1727.2364 -5427.9439 7741.0274 -6564.1266 -8188.5671 -1740.338 2961.8037 -9123.2804 -6122.8657 3142.0566 6124.6983 -7629.8154 -3796.3223 3282.4463 8460.2303 2086.798 -6447.8386 5625.6896 -5146.3386 5255.6235 623.9874 -2217.2798 -7016.7395 -4992.7102 -8503.4095 9446.9379 1373.3452 547.9139 -5721.6245 -4234.2244 5921.305 -9701.5791 -922.0591 -9421.8989 -1812.9044 2042.4364 2412.4411 -6340.1892 2582.9846 7618.4032 -5284.9361 -3807.3723 -4568.9811 -9059.5125 -3445.8022 3793.5939 -4058.8346 4718.2422 -7242.7992 9663.8152 -4267.0352 -3320.6401)))', '(puts (str (multiply 932 -410 230)))', '(puts (str (add -6061 4064 -65 6577 -6834 -8393 -6822 1709 -6795 -9538 2468 -570 1548 9590 8125 -5547 -7026 -9948 6561 -2853 -442 500 -8683 -6233 1544 -930 1534 7257 9315 -8481 3463 3659 8231 -2713 9936 -4883 -8557 -9565 -8169 3211 -6537 -9313 -9325 -2972 -9260 9636 -7140 1364 6015 2807 7020.8096)))', '(puts (str (equal (puts (lowercase "94rlH_r[Xo7Y`QaJe^FXxy8huP=I[qJ[NAkc;tQjd^i0iSFe>lGSXIHx7cAhqZ8onMVaY?Q1mZThjk]3v>fChQ5coA^qU]W0Z[L;Ns2>7v^0vQn]tVXW]<@Twst?V2j4;=_<rszm=kpNJaAL?fvx?gq`i]@gWe?UVJ1u6A?lAU9:2pMM6oxy<vY?QB@Ey6<eVg")) (puts (uppercase "U6Yj:`S>a0p4qwZf5H8x18=SiPOvMr34jO`cEY9b:=KPLBkmcVu`uuzaUPWE:0_KMRwCKNcdpqGk2flp]Hi@ZmAru_iaz:AEcsLym4j3SAPX39fVemBqsFoQw?YhK1Vv=Y:>sPlMQ`0mAk_FkY2knecHME>D7_u1?8Tr@gzWo1[Rx;>R^Vsm^OCo")))))', '(puts (str (divide -7510 206)))', '(puts (concat (str integer) (uppercase "o00000oo00000o")))', '(puts (subtract integer 2357.6438))'],
-    # "expressions":["(set x 15)","(puts (str (subtract x 5)))"]
+    # "expressions":['(set v \"HelloWorld\")', '(puts (concat v \",Student\"))']
     
     
     
@@ -494,7 +554,7 @@ data = {
 # 2024-09-28 15:37:16,454 werkzeug     INFO     127.0.0.1 - - [28/Sep/2024 15:37:16] "POST /lisp-parser HTTP/1.1" 200 -
 # "expressions": ['(puts (replace "hello world" "world" ""))']
 # 2024-09-28 15:37:16,828 werkzeug     INFO     127.0.0.1 - - [28/Sep/2024 15:37:16] "POST /lisp-parser HTTP/1.1" 200 -
-# "expressions": ['(puts (replace (replace (concat (uppercase "WibFzOkX^yTJsydNtgn>J3N") (lowercase "u@sej[xPo;wQ^MagCy1]][<q0m70cilCmmhydepb<y0bf=?O0XIbhH01y`S_Nbs15b]K7XP^EK>Zgo?WOi84paPrurnwD9nm^nYq_2OOS=qS=B4FJPg1400ryg")) "l" "x") "o" "y"))']
+# ////////////////////////////////"expressions": ['(puts (replace (replace (concat (uppercase "WibFzOkX^yTJsydNtgn>J3N") (lowercase "u@sej[xPo;wQ^MagCy1]][<q0m70cilCmmhydepb<y0bf=?O0XIbhH01y`S_Nbs15b]K7XP^EK>Zgo?WOi84paPrurnwD9nm^nYq_2OOS=qS=B4FJPg1400ryg")) "l" "x") "o" "y"))']
 # 2024-09-28 15:37:17,198 werkzeug     INFO     127.0.0.1 - - [28/Sep/2024 15:37:17] "POST /lisp-parser HTTP/1.1" 200 -
 # "expressions": ['(puts (str (not_equal (abs (subtract 5169 -2382)) -8376)))', '(puts (str (gt (multiply 111 -801 -760) 4060)))', '(puts (str (lt (divide 2982 -6617) (multiply -3433 5345))))']
 # 2024-09-28 15:37:17,575 werkzeug     INFO     127.0.0.1 - - [28/Sep/2024 15:37:17] "POST /lisp-parser HTTP/1.1" 200 -
