45c1e98c1b93340da131a0e27a12979a29e12d74
Eric <weemokkachun@gmail.com>
2024-09-28 11:44:19 +0800

    klotski done

---
+++ routes/klotski.py
@@ -0,0 +1,77 @@
+import json
+import logging
+
+from flask import request
+
+from routes import app
+
+logger = logging.getLogger(__name__)
+
+
+class Block:
+    def __init__(self,x,y,width=1,length=1):
+        self.top_left = [x,y]
+        self.x = x
+        self.y = y
+        self.width = width
+        self.length = length
+
+
+
+def solve():
+    for i in range(len(data)):
+    mp = data[i]["board"]
+    move = data[i]["moves"]
+    location = {}
+
+    for j in range(20):
+        cur_y = int(j / 4) #row
+        cur_x = int(j % 4) #column
+        if(not(mp[j] == "@")):
+            if not(mp[j] in location):
+                location[mp[j]] = Block(cur_x, cur_y)
+            else:
+                location[mp[j]].width = max(location[mp[j]].width, cur_x - location[mp[j]].x + 1)
+                location[mp[j]].length = max(location[mp[j]].length, cur_y-location[mp[j]].y + 1)
+
+
+
+    
+    for i in range(int(len(move)/2)):
+        dir = move[2*i+1]
+        if (dir == "N"):
+            location[move[2*i]].y-=1
+        elif(dir == "E"):
+            location[move[2*i]].x+=1
+        elif(dir == "S"):
+            location[move[2*i]].y+=1
+        else:
+            location[move[2*i]].x-=1
+
+
+    location_list = "@"*20
+
+    for key,value in location.items():
+        for i in range(int(value.length)):
+            for j in range(int(value.width)):
+                loc = int(value.x+j+(value.y+i)*4)
+                location_list = location_list[:loc] + key + location_list[loc+1:]
+
+    return location_list
+
+@app.route('/klotski', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+
+
+
+    ans = []
+    for i in range(len(data)):
+        ans.append(solve(data[i]))
+    #ans holds result
+
+    input_value = data.get("input")
+    result = input_value * input_value
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
---
+++ test.py
@@ -0,0 +1,68 @@
+
+
+class Block:
+    def __init__(self,x,y,width=1,length=1):
+        self.top_left = [x,y]
+        self.x = x
+        self.y = y
+        self.width = width
+        self.length = length
+
+
+
+
+data = [
+  {
+    "board": "BCDEBCFGAAFGAAHHI@@J",
+    "moves": "IEIEASBSCSDWDWEWEWFNGNHNINIEAE"
+  },
+  {
+    "board": "BBAACCAADDE@FGG@HIJJ",
+    "moves": "EEDEFNGEHNINJWJWGSESIEHEFSDWASBEBECNDNFNHNJNGWGWESISASDEDEFNHNJNGNIWIWEWEWASJEJEGNENEWAW"
+  },
+  {
+    "board": "BAACDAAE@FF@GHIJGHIJ",
+    "moves": "DSBSFEDEBSAWEWENFNDEDEBEBEASEWEWCWCWFNDNDWJNJNIEBSBSDSDSAEGNGNHWDWDSASCSCEEEESFWGNHNDWJNINBEAS"
+  }
+]
+
+
+for i in range(len(data)):
+    mp = data[i]["board"]
+    move = data[i]["moves"]
+    location = {}
+
+    for j in range(20):
+        cur_y = int(j / 4) #row
+        cur_x = int(j % 4) #column
+        if(not(mp[j] == "@")):
+            if not(mp[j] in location):
+                location[mp[j]] = Block(cur_x, cur_y)
+            else:
+                location[mp[j]].width = max(location[mp[j]].width, cur_x - location[mp[j]].x + 1)
+                location[mp[j]].length = max(location[mp[j]].length, cur_y-location[mp[j]].y + 1)
+
+
+
+    
+    for i in range(int(len(move)/2)):
+        dir = move[2*i+1]
+        if (dir == "N"):
+            location[move[2*i]].y-=1
+        elif(dir == "E"):
+            location[move[2*i]].x+=1
+        elif(dir == "S"):
+            location[move[2*i]].y+=1
+        else:
+            location[move[2*i]].x-=1
+
+
+    location_list = "@"*20
+
+    for key,value in location.items():
+        for i in range(int(value.length)):
+            for j in range(int(value.width)):
+                loc = int(value.x+j+(value.y+i)*4)
+                location_list = location_list[:loc] + key + location_list[loc+1:]
+
+    print(location_list)
